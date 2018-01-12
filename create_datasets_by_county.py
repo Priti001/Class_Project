296211d9080f7d934529336d7949591354018dfe
Alexander Lam <alam@proteus.com>
2018-01-11 17:25:23 -0800

    Update create_datasets_by_county to include lookup table

--- create_datasets_by_county.py
+++ create_datasets_by_county.py
@@ -12,11 +12,24 @@
 #         - 'City_time_series.csv'
 #         - 'cities_crosswalk.csv'
 
+# Output(s)
+# - 'CA_lookup.csv'
+# - directory: 'data_by_county'
+#     - county_1.csv
+#     - ...
+#     - county_n.csv
+
+# In[1]:
+
 
 # Dependencies
 import sys
 import os
 import pandas as pd
+import datetime
+
+
+# In[2]:
 
 
 # Initialize directory structure relative to notebook location
@@ -36,19 +49,57 @@ city_time_series = pd.read_csv(os.path.join(zecon_dataset_loc,files_used[0]))
 combine = city_time_series.merge(crosswalk,right_on = 'Unique_City_ID', left_on = 'RegionName')
 
 
-# Filter to only CA
+# In[3]:
+
+
+def extract_year(x, dateformat = "%Y-%m-%d"):
+    return datetime.datetime.strptime(x, dateformat).timetuple().tm_year
+
+
+# In[4]:
+
+
+# Filter to only: CA
 ca_data = combine.loc[combine['State']=='CA'].dropna(axis='columns',how='all')
 ca_data.to_csv('CA_Zillow_data.csv')
 county_data_dir = 'data_by_county'
+
+# Create Index of files
+template = pd.DataFrame({
+    'County' : [],
+    'State' : 'CA',
+    'File Name' : [],
+    'Min Date': [],
+    'Max Date': [],
+    'Min Price Per Sq Ft': [],
+    'Max Price Per Sq Ft': []
+})
+
+county_profile = template
 
 # Save detail data by county
 try:
     os.stat(county_data_dir)
 except:
     os.mkdir(county_data_dir)
+
 counties = ca_data['County'].unique()
+
 for county in counties:
     this_data = ca_data.loc[ca_data['County'] == county]
-    county_filename = county.replace(' ','')
-    this_data.to_csv(os.path.join(county_data_dir,county_filename+'.csv'))
+    county_filename = county.replace(' ','')+'.csv'
+    this_data.to_csv(os.path.join(county_data_dir,county_filename))
+    this_index_entry = pd.DataFrame({
+            'County' : county,
+            'State' : 'CA',
+            'File Name' : county_filename,
+            'Min Year': this_data['Date'].apply(extract_year).min(),
+            'Max Year': this_data['Date'].apply(extract_year).max(),
+            'Min Price Per Sq Ft': this_data['MedianSoldPricePerSqft_AllHomes'].min(),
+            'Max Price Per Sq Ft': this_data['MedianSoldPricePerSqft_AllHomes'].max()
+        }, index=[0])
+    county_profile = pd.concat([county_profile,this_index_entry],axis=0,ignore_index=True)
+    
+county_profile.to_csv('CA_lookup.csv')
+    
 
